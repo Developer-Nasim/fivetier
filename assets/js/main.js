2d09086ac4720c38b15b47aebd676d2d0d53a46f
Developer-Nasim <ajnasim72@gmail.coom>
2023-01-23 16:03:01 +0600

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,9 @@
     $('.siteBar-btn').click( function (){ 
         $('.mobile-menu').toggleClass('siteBar');   
     });
+ 
 
-
+    //   Set Body padding
     function SetPaddingInBodyForFooter() {
         let body = document.querySelector('body')
         let footer = document.querySelector('footer')
@@ -17,32 +18,57 @@
 
     function MultistepFuncs() { 
         if (document.querySelectorAll('.goBack').length > 0) { 
+            let inps = document.querySelectorAll('input')
             let backBtn = document.querySelector('.goBack')
             let nextBtn = document.querySelector('.goNext')
-            // Next
-            nextBtn.addEventListener('click', () => {
+
+            // Press Enter
+            function PressEnter() {
+                inps.forEach(inp => {
+                    inp.addEventListener("keyup", (event) => {
+                        if (event.keyCode === 13) {
+                            NextStep()
+                        }
+                    });
+                })
+            }
+            PressEnter()
+
+            // Next Step
+            function NextStep() {
                 let Blks = document.querySelector('.stepBlk.active')
                 let NextBlk = Blks.nextElementSibling
                 if (NextBlk) {
-                    Blks.parentElement.parentElement.setAttribute('data-label', NextBlk.dataset.stepLabel)
-                    console.log(NextBlk.dataset.stepLabel)
+                    Blks.parentElement.parentElement.setAttribute('data-label', NextBlk.dataset.stepLabel)  
+                    // Blks.parentElement.style.minHeight = 100+Blks.clientHeight+'px'
                     Blks.classList.remove('active')
                     NextBlk.classList.add('active')
                 }else{
                     window.location.href = window.origin+"/result.html"
                 }
-            })
-            // Back
-            backBtn.addEventListener('click', () => {
+            }
+
+            // Prev Step
+            function PrevStep() {
                 let Blks = document.querySelector('.stepBlk.active')
                 let pastBlk = Blks.previousElementSibling
                 if (pastBlk) {
                     Blks.parentElement.parentElement.setAttribute('data-label', pastBlk.dataset.stepLabel)
+                    // Blks.parentElement.style.minHeight = 100+pastBlk.clientHeight+'px'
                     Blks.classList.remove('active')
                     pastBlk.classList.add('active')
                 }else{
                     alert('Stop');
                 }
+            }
+
+            // Next
+            nextBtn.addEventListener('click', () => {
+                NextStep()
+            })
+            // Back
+            backBtn.addEventListener('click', () => {
+                PrevStep()
             })
         } 
     }
